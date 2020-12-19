455c444b95edc2e64139a21606f4b1802b52494f
Spiritman93 <holms.12@mail.ru>
2020-12-19 10:35:09 +0300

    add update

--- src/by/mts/brest/lawyer/Work.java
+++ src/by/mts/brest/lawyer/Work.java
@@ -1,32 +1,35 @@
 package by.mts.brest.lawyer;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Work {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        //ArrayList<Worker> zpSTO = new ArrayList();
 
         Worker Mehanik = new Worker("Mihalich",5.4f,28);
         System.out.println(Mehanik);
 
-        Mehanik.ZP = 0;
-
         Scanner in = new Scanner(System.in);
-        System.out.print("Input a command:");
-        String num = in.nextInt();
-        for (num == "need work" +  + Mehanik.ZP++) {
-            Mehanik.worked(); }
-        while (num != "need work") {
-            System.out.println("command not recognized, need new command input ");
-        }
+        System.out.print("Input a ZP:");
+        int ZP = in.nextInt();
 
-
-
-
-
-
+        for (ZP != 0; ZP > 0; Mehanik.ZP = ZP) {
+            Mehanik.worked();
+            System.out.println("ZP: Mehanik = " + Mehanik.ZP); }
         in.close();
 
+        String workerDatabase = "C:\\Java\\detector\\worker_file.txt";
+        new File(workerDatabase).getParentFile().mkdirs();
+        FileWriter worker = new FileWriter(workerDatabase);
+        worker.write(Mehanik.ZP+System.getProperty("line.separator"));
+
+        worker.close();
 
 
 
