338dc08a7e414b1e78263d5dad124ed154478b53
Spiritman93 <holms.12@mail.ru>
2021-01-27 21:37:43 +0300

    enum class

---
+++ src/by/mts/brest/lawyer/programEnumSTO.java
@@ -0,0 +1,21 @@
+package by.mts.brest.lawyer;
+
+public class programEnumSTO {
+
+    public static void main(String[] args){
+
+        stoBox active = stoBox.Box1;
+
+        System.out.println("active = " + active);
+            
+        }
+
+    public enum stoBox {
+
+        Box1,
+        Box2,
+        Box3,
+        Box4
+
+    }
+}
